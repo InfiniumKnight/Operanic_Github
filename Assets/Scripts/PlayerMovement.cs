5b969fda34964b4682a9ca3c39ccd46623751982
InfiniumKnight <nasaman1954@gmail.com>
2025-09-17 11:51:39 -0400

    Initial player movement set up using on screen buttons

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerSpeed = 5.0f;
+    private float jumpHeight = 1.5f;
+    private float gravityValue = -9.81f;
+
+    private CharacterController controller;
+    private PlayerInput playerInput;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private InputActionAsset asset;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // Read input
+        Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Jump
+        if (playerInput.actions["Jump"].triggered && groundedPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (input != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = rotation;
+        }
+
+        
+    }
+}
