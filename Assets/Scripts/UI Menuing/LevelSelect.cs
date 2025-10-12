c0fe490dfa2b500c741a6afdeb558dfd6aa148c9
Cel Lewis <149333423+auxpatch@users.noreply.github.com>
2025-10-12 01:51:52 -0400

    LevelSelect UI Setup
    Initiated level setup, issues involve:
    - Buttons from images dont properly bring up other menus

--- Assets/Scripts/UI Menuing/LevelSelect.cs
+++ Assets/Scripts/UI Menuing/LevelSelect.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEditor;
 
 public class LevelSelect : MonoBehaviour
 {
+    public GameObject ArmSelectUI;
+    public GameObject TorsoSelectUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ArmSelectUI.SetActive(false);
+        TorsoSelectUI.SetActive(false);
+    }
+    public void ArmSelect()
+    {
+        SceneManager.LoadScene("LVL_ArtPrototype"); //Should be changed when Art Prototype is not the name of the Arm Level scene anymore
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TorsoSelect()
     {
-        
+        SceneManager.LoadScene("LVL_Torso"); //Takes you to Torso Stage
+    }
+
+    public void PreArmSelect()
+    {
+        ArmSelectUI.SetActive(true); //Activates secondary UI to show gif of arm and start stage
+    }
+
+    public void UndoArmSelect()
+    {
+        ArmSelectUI.SetActive(false); //Undoes the above
+    }
+
+    public void PreTorsoSelect()
+    {
+        TorsoSelectUI.SetActive(true); //Activates secondary UI to show gif of torso and start sage
+    }
+
+    public void UndoTorsoSelect()
+    {
+        TorsoSelectUI.SetActive(false); //Undoes the above
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("quitting");
     }
 }
